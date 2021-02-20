baeff6af7f5e6617b9914d4c2baf6c478db426a7
qwe5507 <70142711+qwe5507@users.noreply.github.com>
2021-02-21 01:10:33 +0900

    Comunity Update

--- src/components/ants/Community.js
+++ src/components/ants/Community.js
@@ -8,6 +8,7 @@ import { Link, Route, useHistory } from 'react-router-dom';
 function Community() {
     let [showMobileHeaderMenu, showMobileHeaderMenuChange] = useState(false);
     let [selectedSwitchValue,selectedSwitchValueChange] = useState(false);
+    let [liked,likedchange] = useState(false);
 
     function toggleHeaderMenu(value) {
       showMobileHeaderMenuChange(value);
@@ -26,8 +27,8 @@ function Community() {
         transition
         left="10%"
         right="0"
-        zIndex="100"
-        w="85%"
+        zIndex="1"
+        w="80%"
         // p={{ y: { xs: "4.5rem", md: "1rem" } }}
 
         align = "space-between"
@@ -50,7 +51,7 @@ function Community() {
           zIndex="-1"
           
         ></Div>
-        <Container w="30rem" d="static" align="center" justify="center" >
+        <Container w="30.5rem" d="static" align="center" justify="center" >
            
 
           {/* <Link to="/">
@@ -74,7 +75,7 @@ function Community() {
               h="2px"
               w="1rem"
               bg="black"
-              rounded="lg"
+              rounded="md"
               style={{
                 transform: `translateY(${showMobileHeaderMenu ? "1" : "-2"
                   }px)rotate(${showMobileHeaderMenu ? "135" : "0"}deg)`,
@@ -245,15 +246,137 @@ function Community() {
              
             <Row>
               <Col size={{ xs: 12, md: 6, lg: 6 }} pos="relative">
+
                 <Div
                   shadow="4"
                   border="1px solid"
                   borderColor="gray200"
-                  rounded="xl"
+                  rounded="lg"
                   p="2rem"
                   m={{ b: { xs: "2rem", md: "1rem" } }}
                 >
-                  <Image
+                    {/* 시작 */}
+                    {/* <Div
+        //   bgImg={cardImg}
+          bgSize="cover"
+          bgPos="center"
+          bgRepeat="no-repeat"
+          p={{ b: "84%" }}
+        /> */}
+        <Div p="1rem" d="flex" align="center" justify="space-between">
+          <Div d="inline-block" align="center" >
+            {/* <Div
+              h="1.5rem"
+              w="1.5rem"
+            //   bgImg={girl2}
+              bgSize="cover"
+              bgPos="center"
+              m={{ r: "1rem" }}
+              rounded="circle"
+            ></Div> */}
+                    <Text
+                    textAlign="left"
+                    textSize="title"
+                    textWeight="500"
+                    fontFamily="secondary"
+                    m={{ b: "1rem" }}
+                    >
+                    Why use Atomize React?Why use Atomize React?
+                    </Text>
+                    <Text
+                    textAlign="left"
+                    textSize="body"
+                    textWeight="500"
+                    fontFamily="secondary"
+                    m={{ b: "1rem" }}
+                    >
+                    Why use Atomize React?Why use Atomize React?
+                    </Text>
+          </Div>
+
+        </Div>
+
+        <Div p="1rem" d="flex" align="center" justify="space-between">
+            <Div d="inline-block" align="center">
+                 <Text
+                    textAlign="left"
+                    textSize="body"
+                    textWeight="700"
+                    fontFamily="secondary"
+                    m={{ b: "0.2rem" }}
+                    >
+                    Lee Jin Gang
+                </Text>
+                <Div d="flex" align="center">
+                    <Icon
+                        transition
+                        name= "Eye"
+                        color= "black"
+                        size="18px"
+                        cursor="pointer"
+                        m={{r : "0.4rem"}}
+                    />
+                    <Text
+                    textAlign="left"
+                    textSize="body"
+                    textWeight="450"
+                    fontFamily="secondary"
+                    m={{r : "1rem"}}
+                    >
+                    4572
+                    </Text>
+                    <Icon
+                        transition
+                        onClick={() => likedchange(!liked)}
+                        name={liked ? "HeartSolid" : "Heart"}
+                        color={liked ? "danger700" : "black"}
+                        size="18px"
+                        cursor="pointer"
+                        m={{r : "0.4rem"}}
+                    />
+                    <Text
+                    textAlign="left"
+                    textSize="body"
+                    textWeight="450"
+                    fontFamily="secondary"
+                    m={{r : "1rem"}}
+                    >
+                    121
+                    </Text>
+                    <Icon
+                        transition
+                        name= "Message"
+                        color= "black"
+                        size="18px"
+                        cursor="pointer"
+                        m={{r : "0.4rem"}}
+                    />
+                    <Text
+                    textAlign="left"
+                    textSize="body"
+                    textWeight="450"
+                    fontFamily="secondary"
+                    m={{r : "1rem"}}
+                    >
+                    121
+                    </Text>
+                </Div>
+            </Div>
+          <Div d="flex" align="center">
+            <Div
+              h="1.5rem"
+              w="1.5rem"
+            //   bgImg={girl2}
+              bgSize="cover"
+              bgPos="center"
+              m={{ r: "1rem" }}
+              rounded="circle"
+            ></Div>
+            <Text textWeight="500">11시간전</Text>
+          </Div>
+        </Div>
+                    {/* 끝 */}
+                  {/* <Image
                     src={logoSketch}
                     h="2.5rem"
                     w="auto"
@@ -272,44 +395,173 @@ function Community() {
                     m={{ b: "1.5rem" }}
                   >
                     게시글 내용1~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                  </Text>
+                  </Text> */}
+
                 </Div>
+
               </Col>
               <Col size={{ xs: 12, md: 6, lg: 6 }} pos="relative">
-                <Div
-                  shadow="4"
-                  border="1px solid"
-                  borderColor="gray200"
-                  bg="white"
-                  rounded="xl"
-                  p="2rem"
-                >
-                  <Image
-                    src={logoReact}
-                    h="2.5rem"
-                    w="auto"
-                    m={{ b: "1rem" }}
-                  />
-                  <Text m={{ b: "0.5rem" }} textSize="heading" textWeight="500">
-                        게시글2
-                  </Text>
-                  <Text
-                    textSize="subheader"
-                    textColor="medium"
-                    p={{ r: "1rem" }}
-                    m={{ b: "1.5rem" }}
-                  >
-                    게시글 내용2~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                  </Text>
-                </Div>
-              </Col>
+
+<Div
+  shadow="4"
+  border="1px solid"
+  borderColor="gray200"
+  rounded="lg"
+  p="2rem"
+  m={{ b: { xs: "2rem", md: "1rem" } }}
+>
+            {/* 시작 */}
+            {/* <Div
+        //   bgImg={cardImg}
+        bgSize="cover"
+        bgPos="center"
+        bgRepeat="no-repeat"
+        p={{ b: "84%" }}
+        /> */}
+        <Div p="1rem" d="flex" align="center" justify="space-between">
+        <Div d="inline-block" align="center" >
+        {/* <Div
+        h="1.5rem"
+        w="1.5rem"
+        //   bgImg={girl2}
+        bgSize="cover"
+        bgPos="center"
+        m={{ r: "1rem" }}
+        rounded="circle"
+        ></Div> */}
+            <Text
+            textAlign="left"
+            textSize="title"
+            textWeight="500"
+            fontFamily="secondary"
+            m={{ b: "1rem" }}
+            >
+            kakao mang hera kakao mang herakakao mang herakakao mang hera
+            </Text>
+            <Text
+            textAlign="left"
+            textSize="body"
+            textWeight="500"
+            fontFamily="secondary"
+            m={{ b: "1rem" }}
+            >
+            why KaKao SiBal Nom ? 
+            </Text>
+        </Div>
+
+        </Div>
+
+        <Div p="1rem" d="flex" align="center" justify="space-between">
+        <Div d="inline-block" align="center">
+        <Text
+            textAlign="left"
+            textSize="body"
+            textWeight="700"
+            fontFamily="secondary"
+            m={{ b: "0.2rem" }}
+            >
+            Lee Jin Soft
+        </Text>
+        <Div d="flex" align="center">
+            <Icon
+                transition
+                name= "Eye"
+                color= "black"
+                size="18px"
+                cursor="pointer"
+                m={{r : "0.4rem"}}
+            />
+            <Text
+            textAlign="left"
+            textSize="body"
+            textWeight="450"
+            fontFamily="secondary"
+            m={{r : "1rem"}}
+            >
+            4572
+            </Text>
+            <Icon
+                transition
+                onClick={() => likedchange(!liked)}
+                name={liked ? "HeartSolid" : "Heart"}
+                color={liked ? "danger700" : "black"}
+                size="18px"
+                cursor="pointer"
+                m={{r : "0.4rem"}}
+            />
+            <Text
+            textAlign="left"
+            textSize="body"
+            textWeight="450"
+            fontFamily="secondary"
+            m={{r : "1rem"}}
+            >
+            121
+            </Text>
+            <Icon
+                transition
+                name= "Message"
+                color= "black"
+                size="18px"
+                cursor="pointer"
+                m={{r : "0.4rem"}}
+            />
+            <Text
+            textAlign="left"
+            textSize="body"
+            textWeight="450"
+            fontFamily="secondary"
+            m={{r : "1rem"}}
+            >
+            121
+            </Text>
+        </Div>
+        </Div>
+        <Div d="flex" align="center">
+        <Div
+        h="1.5rem"
+        w="1.5rem"
+        //   bgImg={girl2}
+        bgSize="cover"
+        bgPos="center"
+        m={{ r: "1rem" }}
+        rounded="circle"
+        ></Div>
+        <Text textWeight="500">백년 전</Text>
+        </Div>
+        </Div>
+            {/* 끝 */}
+        {/* <Image
+            src={logoSketch}
+            h="2.5rem"
+            w="auto"
+            m={{ b: "1rem" }}
+        />
+        <Text 
+        align = "top"
+        m={{ b: "0.5rem" }} textSize="heading" textWeight="500">
+            게시글1
+        </Text>
+
+        <Text
+            textSize="subheader"
+            textColor="medium"
+            p={{ r: "1rem" }}
+            m={{ b: "1.5rem" }}
+        >
+            게시글 내용1~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        </Text> */}
+
+        </Div>
+
+        </Col>
               
               <Col size={{ xs: 12, md: 6, lg: 6 }} pos="relative">
                 <Div
                   shadow="4"
                   border="1px solid"
                   borderColor="gray200"
-                  rounded="xl"
+                  rounded="lg"
                   p="2rem"
                   m={{ b: { xs: "2rem", md: "1rem" } }}
                 >
@@ -339,7 +591,7 @@ function Community() {
                   border="1px solid"
                   borderColor="gray200"
                   bg="white"
-                  rounded="xl"
+                  rounded="lg"
                   p="2rem"
                 >
                   <Image
@@ -367,7 +619,7 @@ function Community() {
                   shadow="4"
                   border="1px solid"
                   borderColor="gray200"
-                  rounded="xl"
+                  rounded="lg"
                   p="2rem"
                   m={{ b: { xs: "2rem", md: "2rem" } }}
                 >
@@ -397,7 +649,7 @@ function Community() {
                   border="1px solid"
                   borderColor="gray200"
                   bg="white"
-                  rounded="xl"
+                  rounded="lg"
                   p="2rem"
                 >
                   <Image
