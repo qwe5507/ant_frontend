790c763eaf45b159360d0480d8fada7b762207b1
mandoo <nattty@gmail.com>
2021-02-25 02:10:59 +0900

    2.25 결제 CRUD

--- src/components/ants/Payment.js
+++ src/components/ants/Payment.js
@@ -1,7 +1,11 @@
-import React from "react"
+import React, {useEffect, connect} from "react"
 import { Button, Container, Text, Div, Dropdown, Anchor, Input, Icon } from "atomize"
+import PaymentApiService from "../../api/PaymentApi";
+import { useHistory, useParams } from 'react-router-dom';
 
 function Payment() {
+    let history = useHistory();
+
     return (
         <div>
              <>
@@ -37,7 +41,34 @@ function Payment() {
                             한달 ￦8,900 으로 황금개미가 되어보세요
                         </Text>
           
-                            <Button
+                            <Button onClick ={() => {
+                                       let uId = localStorage.getItem("userid")
+                                       
+                                        let payment = {
+                                            userId : uId
+                                          }
+                                          PaymentApiService.addPayment(payment)
+                                        .then( res => {
+
+                                          console.log("결제성공");
+                                         
+                                        })
+                                        .catch(err => {
+                                          console.log('savePayment 에러', err);
+                                          alert('구독에 실패했습니다. 관리자에게 문의해주세요');
+                                        });
+                                    
+                                        PaymentApiService.modifyPayment(payment)
+                                        .then( res => {
+                                            alert("구독을 축하드립니다! 백테스트를 체험해 볼까요?")
+                                            history.push('/Backtest');
+                                           
+                                          })
+                                          .catch(err => {
+                                            console.log('modifyPayment 에러', err);
+                                            alert('구독에 실패했습니다. 관리자에게 문의해주세요');
+                                          });
+                            } } 
                                 h="3rem"
                                 w={{ xs: "100%", sm: "11rem" }}
                                 bg="info700"
@@ -65,3 +96,13 @@ function Payment() {
 }
 
 export default Payment;
+/*
+const mapStateToProps = state => {
+    return {
+        userid: state.userid
+        
+    };
+};
+
+export default connect(mapStateToProps)(Payment);
+*/
