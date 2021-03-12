988bb2b5017b47a73e2b86a0ee7c783b760385ed
Youngiyong <youn9354@naver.com>
2021-03-12 10:45:29 +0900

    Stocks 페이지 수정
    Stocks 페이지 수정

--- src/components/ants/Stocks.js
+++ src/components/ants/Stocks.js
@@ -1,25 +1,36 @@
 import React, { useState, useEffect, useRef } from "react"
-import TradingViewWidget, { Themes } from 'react-tradingview-widget';
+import TradingViewWidget, { Themes, symbol } from 'react-tradingview-widget';
 import { Div, Text } from "atomize"
+import { useParams } from 'react-router-dom';
+
     //참고사이트
     //https://github.com/rafaelklaessen/react-tradingview-widget
+
+
+
 function Stocks() {
+
+    let { stocksId } = useParams();
+    console.log(stocksId)
     return (
  
         <Div
-        p={{ t: { xs: "3rem", md: "8rem" } }}
+        p={{ t: { xs: "3rem", md: "rem"} }}
+    
         >
-        <Text tag="h1" textSize="display1" m={{ b: "4rem" }} textAlign="center">
-            종목 차트
+        <Text tag="h1" textSize="display1" m={{ b: "4rem", y:"10rem"}} textAlign="center">
+            종목 번호 : {stocksId} 
         </Text>  
         <div>
         <TradingViewWidget
-         symbol="KOSPI" 
+        //  interval = '5'
+         width="100%"
+         height="600"
+         symbol={stocksId} 
          theme={Themes.DARK}
-         locale="fr"
-         height="2000"
-         autosize
-         />
+         locale="kr"        
+         allow_symbol_change={false}
+     />
         </div>
         </Div>
 
