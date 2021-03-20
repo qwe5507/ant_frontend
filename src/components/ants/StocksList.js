6ac4a25bb09fc5b812506bc6521c9ebd64b03ca9
Youngiyong <youn9354@naver.com>
2021-03-20 18:35:12 +0900

    종목리스트 페이지 수정
    종목리스트 페이지 수정

--- src/components/ants/StocksList.js
+++ src/components/ants/StocksList.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 
 import { Button, Container, Text, Div, Icon, Input, Anchor } from "atomize";
-
-import { Line } from 'react-chartjs-2';
+import styled from 'styled-components';
+import { storiesOf } from '@storybook/react';
 import DataTable from 'react-data-table-component';
 import StockApiService from "../../api/StockApi";
 import MainPnlIndShimmer from "./MainPnlIndShimmer";
 import { useHistory } from 'react-router-dom';
 function StocksList() {
 
-    let [tdList, tdListChange] = useState();
+    let [tdList, tdListChange] = useState([]);
     let history = useHistory();
     const columns = [
         {
@@ -32,34 +32,88 @@ function StocksList() {
             selector: 'change',
             sortable: true
         },
+
     ]
-
-    const items = []
-    let itemsPerPage = 122;
-    for (var i=1; i<=itemsPerPage; i++){
-        var temp = { key : i, name : "Page "+ i }
-        items.push(temp);
-    }
-    const [page, setPage] = useState(0);
-    const from = page * itemsPerPage;
-    const to = (page + 1) * itemsPerPage;
-
-    const options = {
-        filterType: "dropdown",
-        responsive: "stacked",
-        filter: false,
-        download: false,
-        print: false,
-        selectableRows: true,
-        search: false,
-        onRowClick: (row,index)=>{
-          alert(row)
-        }
-    }
-    // function stocksClick(stocksId){
-    //     history.push('/Stocks/' + stockId)
-    // }
     
+    // const TextField = styled.input`
+    //     height: 32px;
+    //     width: 200px;
+    //     border-radius: 3px;
+    //     border-top-left-radius: 5px;
+    //     border-bottom-left-radius: 5px;
+    //     border-top-right-radius: 0;
+    //     border-bottom-right-radius: 0;
+    //     border: 1px solid #e5e5e5;
+    //     padding: 0 32px 0 16px;
+
+    //     &:hover {
+    //         cursor: pointer;
+    //     }    
+    // `;
+
+    // const ClearButton = styled(Button)`
+    //     border-top-left-radius: 0;
+    //     border-bottom-left-radius: 0;
+    //     border-top-right-radius: 5px;
+    //     border-bottom-right-radius: 5px;
+    //     height: 34px;
+    //     width: 32px;
+    //     text-align: center;
+    //     display: flex;
+    //     align-items: center;
+    //     justify-content: center;
+    // `;
+
+
+
+    // const FilterComponent = ({ filterText,  onClear, onFilter }) => (
+    //     <>
+    //         <Input id="search" type="text" placeholder="Filter By Name" aria-label="Search Input" value={filterText} onChange={onFilter} />
+    //         <ClearButton type="button" onClick={onClear}>X</ClearButton>
+    //     </>
+    // );
+
+    // const [filterText, setFilterText] = useState('');
+    // const [resetPaginationToggle, setResetPaginationToggle] = useState(false);
+    // const filteredItems = tdList.filter(item => item.name && item.name.toLowerCase().includes(filterText.toLowerCase()));
+
+    // const subHeaderComponentMemo = useMemo(() => {
+    //     const handleClear = () => {
+    //         if (filterText) {
+    //             setResetPaginationToggle(!resetPaginationToggle);
+    //             setFilterText('');
+    //         }
+    //     };
+    //     return <FilterComponent filterText={filterText} onFilter={ (e) => {setFilterText(e.target.value)}} onClear={handleClear}  />;
+    // }, [filterText, resetPaginationToggle]);
+
+    const click = (row) => {
+        history.push("/Stocks/"+row.code)
+    }
+
+    const conditionalRowStyles = [
+        {
+          when: row => row.change < 0,
+          style: {
+            color: 'rgba(2, 132, 254, 1)',
+            '&:hover': {
+              cursor: 'pointer',
+            },
+          },
+          
+        },
+        {
+            when: row => row.change > 0,
+            style: {
+                color: "rgba(244, 84, 29, 1)",
+                '&:hover': {
+                cursor: 'pointer',
+          },
+        },
+        },
+        
+      ];
+
     useEffect(() => {
         StockApiService.selectByAllStocks()
             .then(res => {
@@ -67,34 +121,37 @@ function StocksList() {
                 var tempData = [];
 
                 for (var i = 0; i < temp.length; i++) {
-                    var dataSet = {
-                        datasets: [
-                          {
-                            datasetStrokeWidth: 10,
-                            type: "line",
-                            borderCapStyle: "round",
-                            borderColor: temp[i]['change'] > 0 ? "rgba(244, 84, 29, 1)" : "rgba(2, 132, 254, 1)",
-                            borderWidth: 3,
-                            backgroundColor: temp[i]['change'] > 0 ? "rgba(251, 207, 208, 1)" : "rgba(179, 218, 255, 1)",
-                            pointHoverRadius: 0,
-                            pointDot: false,
-                            pointRadius: 0,
-                            pointDotRadius: 0
-                          }
-                        ]
-                      }
-                      var wholeData = {
+
+                
+                    //   var dataSet = {
+                    //     datasets: [
+                    //       {
+                    //         datasetStrokeWidth: 10,
+                    //         type: "line",
+                    //         borderCapStyle: "round",
+                    //         borderColor: temp[i]['change'] > 0 ? "rgba(244, 84, 29, 1)" : "rgba(2, 132, 254, 1)",
+                    //         borderWidth: 3,
+                    //         backgroundColor: temp[i]['change'] > 0 ? "rgba(251, 207, 208, 1)" : "rgba(179, 218, 255, 1)",
+                    //         pointHoverRadius: 0,
+                    //         pointDot: false,
+                    //         pointRadius: 0,
+                    //         pointDotRadius: 0,
+                    //       }
+                    //     ]
+                    //   }
+                    var wholeData = {
                         code: temp[i]['code'],
                         name: temp[i]['name'],
                         price: temp[i]['price'],
                         change: temp[i]['change'],
-                        dataSet: dataSet
-                      }
-                      tempData.push(wholeData);
-                      console.log(tempData)
-
+                        // dataSet : dataSet
+                    }
+                    tempData.push(wholeData);
+ 
                 }
+
                 tdListChange(tempData)
+                console.log(tempData)
             })
             .catch(err => {
                 console.log('***********에러', err)
@@ -104,123 +161,112 @@ function StocksList() {
 
     return (
         <Div
-                w="100vw"
-                p={{ t: { xs: "6rem", md: "8rem" } }}
-                overflow="hidden"
-            >
-                <Container
-                    d="flex"
-                    flexDir="column"
-                    align="center"
-                >
-        <Div
-            d="flex"
-            justify="center"
-            p={{ b: "10.5rem", t: "3rem" }}
-            border={{ b: "1px solid" }}
-            borderColor="gray300"
-            w={{ xs: "100%", lg: "59rem" }}
+            w="100vw"
+            p={{ t: { xs: "6rem", md: "8rem" } }}
+            overflow="hidden"
         >
-            <Div
-            d="flex"
-            justify="center"
-                border="1px solid"
-                borderColor="gray200"
-                w={{ xs: "100%", md: "40rem" }}
-                maxW="100%"
-                pos={{ xs: "static", md: "relative" }}
-                m={{ xs: "1rem", md: "1rem" }}
-                top="0"
-                p={{
-                    x: { xs: "2rem", sm: "1.5rem" },
-                    b: { xs: "2rem", sm: "1.5rem" },
-                    t: "1.5rem",
-                }}
-                h="100%"
-                bg="white"
-                shadow="4"
-                rounded="xl"
+            <Container
+                d="flex"
+                flexDir="column"
+                align="center"
             >
-
-
-
-                    {/* 매매내역 */}
+                <Div
+                    d="flex"
+                    justify="center"
+                    p={{ b: "10.5rem", t: "3rem" }}
+                    border={{ b: "1px solid" }}
+                    borderColor="gray300"
+                    w={{ xs: "100%", lg: "59rem" }}
+                >
                     <Div
-                    
-                        p="1rem"
-                        bg="white"
-                        shadow="2"
+                        d="flex"
+                        justify="center"
                         border="1px solid"
                         borderColor="gray200"
+                        w={{ xs: "100%", md: "40rem" }}
+                        maxW="100%"
+                        pos={{ xs: "static", md: "relative" }}
+                        m={{ xs: "1rem", md: "1rem" }}
+                        top="0"
+                        p={{
+                            x: { xs: "2rem", sm: "1.5rem" },
+                            b: { xs: "2rem", sm: "1.5rem" },
+                            t: "1.5rem",
+                        }}
+                        h="100%"
+                        bg="white"
+                        shadow="4"
                         rounded="xl"
-                        w={{ xs: "100%", sm: "100%" }}
-                        m={{ b: "0.5rem" }}
                     >
-                        <Text
-                            m={{ t: "1rem", b: "0.5rem" }}
-                            textAlign="center"
-                            textWeight="800"
-                            textSize="subheader"
-                            fontFamily="ko"
+
+
+
+                        {/* 매매내역 */}
+                        <Div
+
+                            p="1rem"
+                            bg="white"
+                            shadow="2"
+                            border="1px solid"
+                            borderColor="gray200"
+                            rounded="xl"
+                            w={{ xs: "100%", sm: "100%" }}
+                            m={{ b: "0.5rem" }}
                         >
-                            [ 종목내역 ]
+                            <Text
+                                m={{ t: "1rem", b: "0.5rem" }}
+                                textSize="paragraph"
+                                textAlign="center"
+                                textWeight="800"
+                                textSize="subheader"
+                                fontFamily="ko"
+                            >
+                                [ 종목내역 ]
                         </Text>
-                        {tdList
-                            ?
-                            <DataTable
-                                columns={columns}
-                                data={tdList}
-                                options={options}
-                                page={page}
-                                numberOfPages={Math.floor(items.length / itemsPerPage)}
-                                onPageChange={page => setPage(page)}
-                                label={`${from + 1}-${to} of ${items.length}`}
-                                // onRowClicked={ () => stocksClick() }
-                            />
-                            :
-                            <Div>
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                            <MainPnlIndShimmer />
-                             </Div>
-                        }
-                    </Div> {/* 매매내역 종료 */}
+                            {tdList
+                                ?
+                                <DataTable
+                                    columns={columns}
+                                    data={tdList}
+                                    pagination
+                                    // paginationResetDefaultPage={resetPaginationToggle}
+                                    // subHeader
+                                    // subHeaderComponent={subHeaderComponentMemo}
+                                    persistTableHead
+                                    onRowClicked={click}
+                                    conditionalRowStyles={conditionalRowStyles}
+
+
+                                    // onRowClicked={ () => stocksClick() }
+                                />
+                                :
+                                <Div>
+
+                                    <MainPnlIndShimmer />
+                                    <MainPnlIndShimmer />
+                                    <MainPnlIndShimmer />
+                                    <MainPnlIndShimmer />
+                                    <MainPnlIndShimmer />
+                                    <MainPnlIndShimmer />
+                                    <MainPnlIndShimmer />
+                                    <MainPnlIndShimmer />
+                                    <MainPnlIndShimmer />
+                                    <MainPnlIndShimmer />
+
+
+                                </Div>
+                            }
+                        </Div> {/* 매매내역 종료 */}
                     </Div>
 
 
-            </Div>
-        </Container>
+                </Div>
+            </Container>
         </Div>
     )
 }
+
+storiesOf('Filtering', module)
+  .add('Example 1', () => <StocksList />);
 
 export default StocksList;
